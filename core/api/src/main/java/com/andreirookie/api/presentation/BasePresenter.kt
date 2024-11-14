b4db1900ee6a4dbb607f1ce2c8fd94962f46cbff
AndreiRookie <andrey.idem@gmail.com>
2024-11-14 17:11:35 +0400

    BasePresenter: made baseView protected

--- core/api/src/main/java/com/andreirookie/api/presentation/BasePresenter.kt
+++ core/api/src/main/java/com/andreirookie/api/presentation/BasePresenter.kt
@@ -1,7 +1,7 @@
 package com.andreirookie.api.presentation
 
 abstract class BasePresenter<V : BaseView> : IPresenter<V> {
-    var baseView: V? = null
+    protected var baseView: V? = null
     private set
 
     override fun attachView(view: V) {
